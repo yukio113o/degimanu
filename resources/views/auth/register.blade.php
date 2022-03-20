f40a24d64b3df8d7155005a3b11dfd1bc58775a7
yukio <funky.kotetsu@gmail.com>
2022-03-20 07:21:43 +0000

    03 add auth system

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"><a class="text-dark" href="/">degimanu</a></h1>
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
+            
+            @include('_error_card_list')
+
+            <div class="card-text">
+              {{--POST送信処理--}}
+              <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="md-form">
+                  <label for="name">ユーザー名</label>
+                  <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
+                  <small>英数字3〜16文字(登録後の変更はできません)</small>
+                </div>
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}" >
+                </div>
+                <div class="md-form">
+                  <label for="password">パスワード</label>
+                  <input class="form-control" type="password" id="password" name="password" required>
+                </div>
+                <div class="md-form">
+                  <label for="password_confirmation">パスワード(確認)</label>
+                  <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
+                </div>
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
+              </form>
+              {{--POST送信処理--}}
+              <div class="mt-0">
+                <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::resource('products', 'ProductController');
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
