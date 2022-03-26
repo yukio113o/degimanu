8bed944aa30360e8d39ccb0ef0dfb3b2f2dfe162
yukio <funky.kotetsu@gmail.com>
2022-03-26 06:13:23 +0000

    17 manege products

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -1,7 +1,9 @@
 <div class="container ml-3">
     <h2>商品管理</h2>
     <div class="d-flex flex-column">
-        <label class="samazon-sidebar-category-label">商品一覧</label>
+        <label class="samazon-sidebar-category-label">
+            <a href="/dashboard/products">商品一覧</a>
+        </label>
         <label class="samazon-sidebar-category-label">親カテゴリ管理</label>
         <label class="samazon-sidebar-category-label">
             <a href="/dashboard/categories">カテゴリ管理</a>
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::put('users/mypage/password', 'UserController@update_password')->name('myp
 
 Route::post('products/{product}/reviews', 'ReviewController@store');
 Route::get('products/{product}/favorite', 'ProductController@favorite')->name('product.favorite');
-Route::resource('products', 'ProductController');
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/{product}', 'ProductController@show')->name('products.show');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -33,6 +34,7 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
       Route::get('login', 'Dashboard\Auth\LoginController@showLoginForm')->name('login');
       Route::post('login', 'Dashboard\Auth\LoginController@login')->name('login');
       Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
+      Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
   });
 
  if (env('APP_ENV') === 'local') {
