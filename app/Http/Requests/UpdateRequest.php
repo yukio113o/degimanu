aa4d8885f968dcc893023ff17950e03b17be5c77
hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-08-16 15:05:00 +0900

    カテゴリーの各バリデーション処理→フォームリクエスト化

--- app/Http/Requests/UpdateRequest.php
+++ app/Http/Requests/UpdateRequest.php
@@ -11,7 +11,7 @@ class UpdateRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -21,7 +21,15 @@ class UpdateRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|unique:categories',
+            'description' => 'required'
+        ];
+    }
+
+    public function messages(): array
     {
         return [
             'name.required' => 'カテゴリ名は必須です。',
